050c44e3885494ae95c2139890f92f9a9a760e17
אלירון <Elironbarel123@gmail.com>
2025-04-20 20:05:19 +0300

    Commiting the whole code

--- react-main/src/components/AddCustomer.js
+++ react-main/src/components/AddCustomer.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee(props) {
-    const [name, setName] = useState(props.name);
-    const [role, setRole] = useState(props.role);
-
-    const [show, setShow] = useState(false);
+export default function AddCustomer(props) {
+    const [name, setName] = useState('');
+    const [industry, setIndustry] = useState('');
+    const [show, setShow] = useState(props.show);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -14,27 +13,28 @@ function EditEmployee(props) {
     return (
         <>
             <button
-                onClick={handleShow}
-                className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+                onClick={props.toggleShow}
+                className="block m-2 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
             >
-                Update
+                + Add Customer
             </button>
 
             <Modal
-                show={show}
+                show={props.show}
                 onHide={handleClose}
                 backdrop="static"
                 keyboard={false}
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Update Employee</Modal.Title>
+                    <Modal.Title>Add Customer</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <form
                         onSubmit={(e) => {
-                            handleClose();
                             e.preventDefault();
-                            props.updateEmployee(props.id, name, role);
+                            setName('');
+                            setIndustry('');
+                            props.newCustomer(name, industry);
                         }}
                         id="editmodal"
                         className="w-full max-w-sm"
@@ -45,13 +45,14 @@ function EditEmployee(props) {
                                     className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                                     for="name"
                                 >
-                                    Full Name
+                                    Name
                                 </label>
                             </div>
                             <div className="md:w-2/3">
                                 <input
-                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                                     id="name"
+                                    placeholder="Google"
                                     type="text"
                                     value={name}
                                     onChange={(e) => {
@@ -64,19 +65,20 @@ function EditEmployee(props) {
                             <div className="md:w-1/3">
                                 <label
                                     className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                                    for="role"
+                                    for="industry"
                                 >
-                                    Role
+                                    Industry
                                 </label>
                             </div>
                             <div className="md:w-2/3">
                                 <input
-                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                                    id="role"
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="industry"
+                                    placeholder="Computing"
                                     type="text"
-                                    value={role}
+                                    value={industry}
                                     onChange={(e) => {
-                                        setRole(e.target.value);
+                                        setIndustry(e.target.value);
                                     }}
                                 />
                             </div>
@@ -86,7 +88,7 @@ function EditEmployee(props) {
                 <Modal.Footer>
                     <button
                         className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
-                        onClick={handleClose}
+                        onClick={props.toggleShow}
                     >
                         Close
                     </button>
@@ -94,12 +96,10 @@ function EditEmployee(props) {
                         className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
                         form="editmodal"
                     >
-                        Update
+                        Add
                     </button>
                 </Modal.Footer>
             </Modal>
         </>
     );
 }
-
-export default EditEmployee;
